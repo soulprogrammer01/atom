411fa8a37d8991b15c4e3f2712b12096483b43c4
sameeksha <itsmesameeksha1317@gmail.com>
2020-03-30 19:30:08 +0530

    third

---
+++ form1.py
@@ -0,0 +1,109 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+    <style media="screen">
+      a{
+        color:blue;
+      }
+      .c{
+        line-height:0.4;
+      }
+
+    </style>
+  </head>
+  <body>
+
+    <ul>
+      <li><a href="#">Pizza Zone Account</a></li>
+      <li><a href="#">Home</a></li>
+      <li><a href="#">Contact Us</a></li>
+          </ul>
+      <p style="font-size:19px"><b>azahar Pizza</b></p>
+      <hr>
+      <p>asdfghjklzxcvbnmwertyuio</p>
+      <hr >
+<div class="c">
+
+
+      <p>Small      	Rs99</p>
+      <p>Medium	Rs199</p>
+      <p>Large	Rs399</p>
+      <p>X-Large	Rs599</p>
+      <p>Toppings</p>
+      <p>Rs20	Each</p>
+
+      <p>free delivery</p>
+
+      </div>
+<form action="/action_page.php" method="get">
+
+        <label for="">Your price</label>
+        <br>
+        <input type="text" name="" value="">
+      </form>
+
+      <br>
+      <fieldset>
+        <legend>Personal Information</legend>
+<label for="">name</label>
+        <input type="text" name="" value=""><br>
+    <label for="">Account number</label>
+        <input type="text" name="" value=""><br>
+    <label for="">Phone number</label>
+        <input type="text" name="" value=""><br>
+    <label for="">Date of birth</label>
+        <input type="text" name="" value=""><br>
+
+
+      </fieldset>
+      <fieldset>
+        <legend>Pizza size& Crust</legend>
+        <select class="" name="">
+
+        </select>
+      </fieldset>
+      <fieldset>
+        <legend>Cheeses</legend>
+        <label for="">Mozzarella</label>
+        <input type="radio" name="" value="">
+        <label for="">Cheddar</label>
+        <input type="radio" name="" value="">
+        <label for="">Cream cheese</label>
+        <input type="radio" name="" value="">
+      </fieldset>
+      <fieldset>
+        <legend>Sauces</legend>
+        <label for="">Pizza Sauce</label>
+        <input type="radio" name="" value="">
+        <label for="">BBQ sauce</label>
+        <input type="radio" name="" value="">
+        <label for="">Garlic Sauce</label>
+        <input type="radio" name="" value="">
+      </fieldset>
+      <fieldset>
+<legend>Special Instructions</legend>
+        <textarea name="name" rows="8" cols="80"></textarea>
+      </fieldset>
+<button type="button" name="button">PLACE YOUR ORDER</button>
+<button type="button" name="button">START OVER</button>
+<fieldset>
+  <label for="">Choose Toppings - Rs20 Each</label>
+  <label for=""> Black Olive</label>
+  <input type="checkbox" name="" value=""><br>
+  <label for=""> Onion</label>
+  <input type="checkbox" name="" value=""><br>
+  <label for=""> Red Paprika</label>
+  <input type="checkbox" name="" value=""><br>
+  <label for=""> Golden Corn</label>
+  <input type="checkbox" name="" value=""><br>
+  <label for=""> Grilled Mushroom</label>
+  <input type="checkbox" name="" value=""><br>
+  <label for=""> Paneer</label>
+  <input type="checkbox" name="" value="">
+</fieldset>
+
+
+  </body>
+</html>
